2ddfacfcf291dd42419260748a69e26d07eeb00b
Ranjan Raj Thakur <142471580+RanjanKumarraj@users.noreply.github.com>
2023-08-21 21:07:52 +0530

    Area of triangle

---
+++ Area of triangle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	float b,h,x;
+	printf("Enter Base and height of a triangel-\n");
+	scanf("%f\n%f",&b,&h);
+	x=0.5*b*h;
+	printf("Area Of triangle-%f",x);
+	return 0;
+}
