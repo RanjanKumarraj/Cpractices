02a987a3f4fdba8a4c055aa75b1e5df14941ad0a
Ranjan raj <142471580+RanjanKumarraj@users.noreply.github.com>
2023-08-21 18:41:37 +0530

    Simple Add
    Using C lang create a Add programe

---
+++ simple add.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int num1,num2;
+	printf("Enter a no-1\n");
+	scanf("%d",&num1);
+	printf("Enter Second no-\n");
+	scanf("%d",&num2);
+	printf("Sum of two no is %d",num1+num2);
+	return 0;
+	
+}
