883d386b0340425824567b159967eff339704dc9
Ranjan Raj Thakur <142471580+RanjanKumarraj@users.noreply.github.com>
2023-08-21 22:02:35 +0530

    Find Simple Intrest

---
+++ si value.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	float p,r,n,si;
+	printf("Enter thr value of p-\n");
+	scanf("%f",&p);
+	printf("Enter the value of R-\n");
+	scanf("%f",&r);
+	printf("Enter the value of n-\n");
+	scanf("%f",&n);
+	si=(p*r*n)/100;
+	printf("The simple intrest is %f\n",si);
+	
+	return 0;
+}
