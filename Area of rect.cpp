129b3a89b0f85f90170f529d830aa30ba4ebea24
Ranjan Raj Thakur <142471580+RanjanKumarraj@users.noreply.github.com>
2023-08-21 20:56:05 +0530

    Area of rect

---
+++ Area of rect.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float l,b;
+	printf("Enter lenth and breadth\n");
+	scanf("%f\n%f",&l,&b);
+	printf("Area of rectangle-%f",l*b);
+	return 0;
+}
