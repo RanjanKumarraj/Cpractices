77c94ce7254d305f36c800b595f90341fce33c46
Ranjan raj <142471580+RanjanKumarraj@users.noreply.github.com>
2023-08-21 18:33:30 +0530

    Self Intro In C-lang
    I am Creating a programe that introduse my self

---
+++ self-intro.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello i'm Ranjan Raj Thakur\n");
+	printf("I am passed from 10th and 12th from CBSE board\n");
+	printf("Currently I am studying in KIET group of Institution\n");
+	
+	return 0;
+	
+}
