362ac3d4777b658e1cc19e44bbf86a0dba770931
Ranjan raj <142471580+RanjanKumarraj@users.noreply.github.com>
2023-08-21 19:12:20 +0530

    cal-sum,sub,mul,div

---
+++ sum+sub+mul+div.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int num1,num2;
+	
+	printf("Enter a no-1\n");
+	scanf("%d",&num1);
+	printf("Enter Second no-\n");
+	scanf("%d",&num2);
+	printf("Sum of two no is %d\n\n",num1+num2);
+	
+	
+	
+	printf("Enter a no-1\n");
+	scanf("%d",&num1);
+	printf("Enter Second no-\n");
+	scanf("%d",&num2);
+	printf("Sub of two no is %d\n\n",num1-num2);
+	
+		
+	printf("Enter a no-1\n");
+	scanf("%d",&num1);
+	printf("Enter Second no-\n");
+	scanf("%d",&num2);
+	printf("Multiplay of two no is %d\n\n",num1*num2);
+	
+		
+	printf("Enter a no-1\n");
+	scanf("%d",&num1);
+	printf("Enter Second no-\n");
+	scanf("%d",&num2);
+	printf("Div of two no is %d\n\n",num1/num2);
+	
+	
+	printf("\n\n\t\t\t THANK'S YOU HAVE A NICE DAY");
+
+	
+
+	return 0;
+	
+}
+
