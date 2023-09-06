3cb6be9c49e2f3b0e508ffa18eba61ecea0b43f0
Ranjan Raj Thakur <142471580+RanjanKumarraj@users.noreply.github.com>
2023-09-06 08:39:11 +0530

    Add files via upload

---
+++ positive,nagative(if else).cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{   
+    int x;
+	printf("enter any no-");
+	scanf("%d",&x);
+	if(x>=0)
+	{
+		printf("%d is positive no.",x);
+		
+	}
+	else
+	{
+		printf("%d is nagative no.",x);
+	}
+	return 0;
+}
