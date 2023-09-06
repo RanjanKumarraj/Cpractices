28641b75492de0bb7eeb9ed2ec9c046b42f7bc81
Ranjan Raj Thakur <142471580+RanjanKumarraj@users.noreply.github.com>
2023-09-06 08:32:32 +0530

    Add files via upload

---
+++ under 100(if else).cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{   
+    int x;
+	printf("enter any no-");
+	scanf("%d",&x);
+	if(x>100)
+	{
+		printf("%d is the greater than 100",x);
+		
+	}
+	else
+	{
+		printf("%d is under 100",x);
+	}
+	return 0;
+}
