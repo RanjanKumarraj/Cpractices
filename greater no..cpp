26a9f1eedbadd3f27ae29173f1c4f5a8e4ac1997
Ranjan Raj Thakur <142471580+RanjanKumarraj@users.noreply.github.com>
2023-09-06 08:51:17 +0530

    Add files via upload

---
+++ greater no..cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{   
+    int x,y;
+	printf("enter any two no-");
+	scanf("%d\n%d",&x,&y);
+	if(x>y)
+	{
+		printf("%d is greater than %d.",x,y);
+		
+	}
+	
+	return 0;
+}
